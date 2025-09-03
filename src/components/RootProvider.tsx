85bf0220e509e11cb55fb78d85f4bab2865f6ca5
shivateja2404 <shivatejagondala@gmail.com>
2025-09-03 22:54:44 +0530

    .

--- src/components/RootProvider.tsx
+++ src/components/RootProvider.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "../contexts/ThemeContext";
 import {SpeedInsights} from '@vercel/speed-insights/next'
